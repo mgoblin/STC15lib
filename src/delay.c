21b7fd38ef9517eef127c05451fc492fb78de587
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-03 06:47:51 +0300

    delay_ms now use master clock divider to adjust delay value

--- src/delay.c
+++ src/delay.c
@@ -4,12 +4,11 @@
 
 void delay_ms(uint16_t ms)
 {
-    //uint8_t cpu_freq_divider = 1 << and_mask(CLK_DIV, 0b00000111);
-    //uint16_t ms_div = ms / cpu_freq_divider;
+    uint16_t ms_div = ms / get_frequency_divider();
     unsigned int i;
     do
     {
        i = (get_master_clock_frequency() / 13000);
        while (--i); //14T per loop
-    } while (--ms);
+    } while (--ms_div);
 }
--- src/frequency_example.c
+++ src/frequency_example.c
@@ -12,12 +12,13 @@ void main()
 {
     uart_init(9600);
 
-    //CLK_DIV |= 0b00000111;
+    CLK_DIV |= 0b00000111;
 
     while (1)
     {
-        printf_tiny("CPU freq is %d", get_master_clock_frequency_high_part());
-        printf_tiny("%d Hz\n", get_master_clock_frequency_low_part());
+        //printf_tiny("Master clock freq is %d", get_master_clock_frequency_high_part());
+        //printf_tiny("%d Hz\n", get_master_clock_frequency_low_part());
+        //printf_tiny("Master clock divider is %d\n", get_frequency_divider());
 
         LED = 1;
         delay_ms(500);
