38f37803bf8fc324c3ddb393e5ae9236a08a90a6
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-05 06:26:19 +0300

    low level implementation timer example done

--- src/timer.c
+++ src/timer.c
@@ -1,5 +1,26 @@
+#include <sys.h>
 #include <timer.h>
 
-void init_timer(const timer_id timerid, const timer_type timertype, timer_mode timermode)
+void init_timer0(const timer_type timertype, timer_mode timermode)
 {
+    if (timertype == TIMER)
+    {
+
+    } 
+    else 
+    {
+
+    }
+}
+
+void init_timer(const timer_id timerid, const timer_type timertype, timer_mode timermode)
+{ 
+    if (timerid == TIMER0)
+    {
+        init_timer0(timertype, timermode);
+    } 
+    else if (timerid == TIMER1) 
+    {
+
+    }
 }
--- src/timer_example.c
+++ src/timer_example.c
@@ -1,6 +1,33 @@
-#include <timer.h>
+#include <sys.h>
+
+#include <stdint.h>
+
+#define LED P10
+
+void reload_timer() 
+{
+    TMOD = 0x00;
+    TL0 = 0x1f;
+    TH0 = 0xff;
+}
+
+void run_timer()
+{
+    TF0 = 0;
+    TR0 = 1;
+
+    while(!TF0)
+    {
+    }
+}
 
 void main()
 {
-    init_timer(TIMER0, COUNTER, MODE0);
+    LED = !LED;
+    
+    for(uint16_t i = 0; i < 128 * 10; i++)
+    {
+        reload_timer();
+        run_timer();
+    }
 }
