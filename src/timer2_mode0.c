c42167cb6677959b23ef124a37f4480a7ad27f33
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-03 10:07:11 +0300

    use interrupts module in timer2

--- src/timer2_mode0.c
+++ src/timer2_mode0.c
@@ -3,10 +3,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <bits.h>
+#include <interrupts.h>
 
 void timer2_mode0_12T_init()
 {
-    EA = 1; // all interrupts enabled
+    enable_mcu_interrupts();
     //bit_clr(AUXR, 4); // clear T2 run flag
     //bit_clr(AUXR, 3); // clear T/C flag for set timer mode
     //bit_clr(AUXR, 2); // clear T2 T2x12 flag for set 12T mode
@@ -17,7 +18,7 @@ void timer2_mode0_12T_init()
 
 void timer2_mode0_1T_init()
 {
-    EA = 1; // all interrupts enabled
+    enable_mcu_interrupts();
     //bit_clr(AUXR, 4); // clear T2 run flag
     //bit_clr(AUXR, 3); // clear T/C flag for set timer mode
     //bit_set(AUXR, 2); // set T2 T2x12 flag for set 1T mode
