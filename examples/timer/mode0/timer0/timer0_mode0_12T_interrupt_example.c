895badd75013441dcfa673d04430568f95063086
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-09 22:09:15 +0300

    simplify timer0_mode0_12T_interrupt_example.c

--- examples/timer/mode0/timer0/timer0_mode0_12T_interrupt_example.c
+++ examples/timer/mode0/timer0/timer0_mode0_12T_interrupt_example.c
@@ -1,5 +1,4 @@
 #include <sys.h>
-#include <delay.h>
 #include <timer.h>
 
 #define LED P10
@@ -15,9 +14,5 @@ void main()
 
     timer0_start(0xffff);
 
-    delay_ms(2000);
-
-    timer0_stop();
-
-    delay_ms(2000);
+    while(1){};
 }
