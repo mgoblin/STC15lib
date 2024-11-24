06b8a6939bff4f77a2c720de3dc0da9373f6c8e8
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-24 05:29:43 +0300

    reset example implemented

---
+++ examples/reset/reset_example.c
@@ -0,0 +1,21 @@
+/**
+ * How to software reset MCU
+ * 
+ * Without reset call this example do LED blinking.
+ * But reset doesnt execute blink code in while cycle and LED not blinking.
+ */
+#include <reset.h>
+#include <delay.h>
+
+#define LED P10
+void main()
+{
+    reset();
+
+    while (1)
+    {
+        LED = !LED;
+        delay_ms(500);
+    }
+    
+}
