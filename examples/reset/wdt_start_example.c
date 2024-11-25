27982ad4b9746abf4dec064a32b6ae7b283af711
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-25 20:42:10 +0300

    wdt_start_example simplified

--- examples/reset/wdt_start_example.c
+++ examples/reset/wdt_start_example.c
@@ -2,8 +2,8 @@
  * How to start Watch Dog Timer (WDT)
  * 
  * Start wdt once with 4 sec period. 
- * Then LED off, wait 2 sec and start blinking.
- * After 4 sec WDT restart MCU and LED if of on 2 sec 
+ * Then LED off, wait 3 sec and start blinking.
+ * After 4 sec WDT restart MCU and LED if of/on 1 sec (3 times led on)
  */
 #include <reset.h>
 #include <delay.h>
@@ -14,14 +14,11 @@
 
 void main()
 {
-    if (!is_wdt_started())
-    {
-        wdt_init(0x06); // 4.19 sec
-        wdt_start();
-    } 
+    wdt_init(0x06); // 4.19 sec
+    wdt_start();
 
     LED = OFF;
-    delay_ms(2000);
+    delay_ms(3000);
 
     while (1) 
     {
