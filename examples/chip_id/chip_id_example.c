25cdc92c3febf7d432d5ec8f4f04372642ddf4cb
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-27 08:51:25 +0300

    chip_id_example refactored

--- examples/chip_id/chip_id_example.c
+++ examples/chip_id/chip_id_example.c
@@ -3,18 +3,31 @@
 #include <stdio.h>
 #include <uart.h>
 
+const uint8_t *cid2 = &chip_id;
+uint8_t cid[CHIP_ID_SIZE];
+
+void print_chip_id(const uint8_t *chip_id)
+{
+    for(uint8_t i = 0; i < CHIP_ID_SIZE; i++)
+    {
+        printf_tiny("%x", chip_id[i]);
+    }
+    printf_tiny("\n");
+}
+
 void main()
 {
     uart1_init(9600);
 
+    get_chipid(cid);
+
     while (1)
     {
-        const uint8_t *cid2 = &chip_id;
-        for(uint8_t i = 0; i < CHIP_ID_SIZE; i++)
-        {
-            printf_tiny("%x", cid2[i]);
-        }
-        printf_tiny("\n");
+        printf_tiny("   chip_id bytes: ");
+        print_chip_id(cid2);
+
+        printf_tiny("get_chipid bytes: ");
+        print_chip_id(cid);
     }
     
 }
