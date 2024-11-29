7f51e02d80498ceff92c735c20592aa0ae89cd4d
Michael Golovanov <mike.golovanov@gmail.com>
2024-11-29 22:10:58 +0300

    wdt_clear_example verified

---
+++ examples/wdt/wdt_clear_example.c
@@ -0,0 +1,26 @@
+/**
+ * How to use WDT with clear
+ * 
+ * WDT started with 4 sec period and clear every second. 
+ * MCU not restarted and LED will blinking.  
+ * 
+ */
+#include <wdt.h>
+
+#include <delay.h>
+
+#define LED P10
+
+void main()
+{
+    wdt_init(0x06); //// 4.19 sec
+    wdt_start();
+
+    while (1) 
+    {
+        LED = !LED;
+        delay_ms(500);
+
+        wdt_clear();
+    }
+}
