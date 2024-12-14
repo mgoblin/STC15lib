238d87b7f392beae156ae1ead78315d96837074e
Michael Golovanov <mike.golovanov@gmail.com>
2024-12-14 07:23:42 +0300

    timer_ms_to_ticks_usafe now use define instead of func decl

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['timer_5fms_5fto_5fticks_5fusafe_0',['timer_ms_to_ticks_usafe',['../group__timer__to__ms.html#gab094675112fd2b0c411fa7cd881c648a',1,'timer_ms_to_ticks_usafe(uint16_t ms, timer_clock_divider_t timer_clock_divider):&#160;timer_to_ms_common.c'],['../group__timer__to__ms.html#gab094675112fd2b0c411fa7cd881c648a',1,'timer_ms_to_ticks_usafe(uint16_t ms, timer_clock_divider_t timer_clock_divider):&#160;timer_to_ms_common.c']]]
+  ['uart1_5freceive_5fbyte_0',['uart1_receive_byte',['../group__uart.html#ga5f66ea2c05ce23684284f338d9067179',1,'uart1_receive_byte():&#160;uart.c'],['../group__uart.html#ga5f66ea2c05ce23684284f338d9067179',1,'uart1_receive_byte():&#160;uart.c']]],
+  ['uart1_5fsend_5fbyte_1',['uart1_send_byte',['../group__uart.html#gaee0e2768c65fa6dfe5e87991f729c6d0',1,'uart1_send_byte(uint8_t data):&#160;uart.c'],['../group__uart.html#gaee0e2768c65fa6dfe5e87991f729c6d0',1,'uart1_send_byte(uint8_t data):&#160;uart.c']]]
 ];
--- include/timer_to_ms_common.h
+++ include/timer_to_ms_common.h
@@ -68,11 +68,6 @@
  */
 #define timer_ticks_to_ms_usafe(ticks, timer_divider, frequency_divider_scale) (((1UL + (uint32_t)ticks) / ((get_master_clock_frequency_high_part() / timer_divider) >> frequency_divider_scale)))
 
-// 1T mode timer0 mode0 - ticks = time * SysClk - 1
-// for 1 ms (10^-3) = get_master_clock_frequency_high_part()/10 - 1 = 11059 - 1 = 11058 = 0x2b32
-// measurement is 0x2b4d, delta = 27
-// get_master_clock_frequency_high_part()/10 + 26
-
 /**
  * @brief Convert milliseconds to timer ticks.
  * @details 
@@ -87,6 +82,6 @@
  * 
  * @ingroup timer_to_ms
  */
-uint16_t timer_ms_to_ticks_usafe(uint16_t ms, timer_clock_divider_t timer_clock_divider);
+#define timer_ms_to_ticks_usafe(ms, timer_clock_divider) (ms * ((get_master_clock_frequency_high_part() + 26) >> get_frequency_divider_scale()) / timer_clock_divider)
 
 #endif
