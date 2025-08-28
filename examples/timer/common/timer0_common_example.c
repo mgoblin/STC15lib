ecb53007db98500437c8b8f6a42da5049a5b140b
Michael Golovanov <mike.golovanov@gmail.com>
2025-08-28 21:29:26 +0300

    Build examples hex. timer0_common_example

--- examples/timer/common/timer0_common_example.c
+++ examples/timer/common/timer0_common_example.c
@@ -17,7 +17,7 @@ void print_timer0_mode_and_divider()
 {
     uint8_t mode = get_timer0_mode();
     timer_clock_divider_t divider =  get_timer0_clock_divider();
-    printf_tiny("Timer mode is %d and divider is %d\n", mode, divider);
+    printf_tiny("Timer mode is %d and divider is %d\r\n", mode, divider);
 }
 
 void main()
@@ -26,7 +26,7 @@ void main()
 
     while (1)
     {
-        printf_tiny("========================\n");
+        printf_tiny("========================\r\n");
 
         timer0_mode0_12T_init();
         print_timer0_mode_and_divider();
@@ -37,8 +37,8 @@ void main()
         timer0_mode1_12T_init();
         print_timer0_mode_and_divider();
         
-        printf_tiny("========================\n");
+        printf_tiny("========================\r\n");
 
-        delay_ms(2000);
+        delay_ms(1000);
     }
 }
