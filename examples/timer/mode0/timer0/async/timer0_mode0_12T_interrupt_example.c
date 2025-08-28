02bad8c8c3ff7f4fd706e339d018494370de550b
Michael Golovanov <mike.golovanov@gmail.com>
2025-08-28 21:57:25 +0300

    Build examples hex. timer0_mode0_12T_interrupt_example

--- examples/timer/mode0/timer0/async/timer0_mode0_12T_interrupt_example.c
+++ examples/timer/mode0/timer0/async/timer0_mode0_12T_interrupt_example.c
@@ -1,5 +1,8 @@
 /**
- * How to start timer0
+ * How to start timer0.
+ * 
+ * Timer0 started with interrupt handler. Interrupt hanlder doing LED blinking.
+ * After start timer main program going to endless waiting, but timer will ticks.
  */
 #include <timer0_mode0.h>
 
