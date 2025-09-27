8291927d5d159f1f658d8ee1f343cc3f870cda96
Michael Golovanov <mike.golovanov@gmail.com>
2025-09-27 09:15:10 +0300

    UART1 mode0 stub

--- src/uart1_mode0.c
+++ src/uart1_mode0.c
@@ -2,5 +2,24 @@
 
 void uart1_mode0_init()
 {
-    
+    // Configure UART1 in Mode 0
+    SCON = 0x50; // 8-bit data, no parity, 1 stop bit
+}
+
+
+void uart1_mode0_send_byte(uint8_t byte)
+{
+    if(TI==0)
+    {
+        SBUF = byte;
+    }
+}
+
+void uart1_mode0_receive_byte(uint8_t *byte)
+{
+    if(RI==1)
+    {
+        *byte = SBUF;
+        RI = 0;
+    }
 }
