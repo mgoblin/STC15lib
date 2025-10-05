1cc8136ebb43a5c3d42346bab8e225d770a002c8
Michael Golovanov <mike.golovanov@gmail.com>
2025-10-05 09:03:23 +0300

    add UART1 mode2 module docs

--- include/uart1_mode2.h
+++ include/uart1_mode2.h
@@ -15,7 +15,19 @@
  * @details Functions and data structures for UART1 communication in Mode 2.
  * 
  * Mode 2 provides asynchronous communication with fixed baud rate and 9-bit data.
+ * 9-N-1.
  * 
+ * The baud rate is fixed at 1/32 (or 1/64) the System clock cycle.
+ * 
+ * @attention Baud rate is biased by frequency divider
+ * 
+ * @see uart1_send_byte()
+ * @see uart1_receive_byte()
+ * @see is_uart1_send_byte_complete()
+ * @see is_uart1_receive_byte_complete()
+ 
+ * 
+ * @author Michael Golovanov
  */
 
 typedef enum {
