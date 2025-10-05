392f99cebe6a238d96b5e574e6e52859d9d8cbaf
Michael Golovanov <mike.golovanov@gmail.com>
2025-10-05 18:38:47 +0300

    uart1_9bit_shared routines are macro

--- include/uart1_9bit_shared.h
+++ include/uart1_9bit_shared.h
@@ -6,8 +6,18 @@
 #include <sys.h>
 #include <uart1_shared.h>
 
-void uart1_send_9bit(uint8_t byte, uint8_t nineth);
+#define uart1_send_9bit(byte, nineth)                       \
+do {                                                        \
+    TB8 = (uint8_t)(nineth & 0x01);                         \
+    uart1_send_byte(byte);                                  \
+} while (0)
 
-void uart1_receive_9bit(uint8_t *byte_ptr, uint8_t *nineth_ptr);
+#define uart1_receive_9bit(byte_ptr, nineth_ptr)            \
+do {                                                        \
+    while(!RI);                                             \
+    *byte_ptr = SBUF;                                       \
+    *nineth_ptr = (uint8_t)(RB8 & 0x01);                    \
+    RI=0;                                                   \
+while (0)    
 
 #endif
