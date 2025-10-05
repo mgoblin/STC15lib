7cf3acb877d5c0f5455d80c787ed319083bd9c4f
Michael Golovanov <mike.golovanov@gmail.com>
2025-10-05 10:25:59 +0300

    uart1_mode3_timer2_12t_baudrate_t name fixed

--- src/uart1_mode3_timer2_12T.c
+++ src/uart1_mode3_timer2_12T.c
@@ -5,12 +5,12 @@ void uart1_mode3_timer2_12T_init(uart1_pins_t pins)
 
 }
 
-void uart1_mode3_timer2_12T_start(uart1_mode2_timer2_12t_baudrate_t baudrate)
+void uart1_mode3_timer2_12T_start(uart1_mode3_timer2_12t_baudrate_t baudrate)
 {
 
 }
 
-void uart1_mode3_timer2_12T_start_ext(uart1_mode2_timer2_12t_baudrate_t baudrate)
+void uart1_mode3_timer2_12T_start_ext(uart1_mode3_timer2_12t_baudrate_t baudrate)
 {
 
 }
