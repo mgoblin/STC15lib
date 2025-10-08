5e3ae4e87aa2914f8aaf5961d4fa3bde24de616d
Michael Golovanov <mike.golovanov@gmail.com>
2025-10-08 20:02:58 +0300

    UART1 mode3 timer2 12T send with parity bit example

---
+++ examples/uart/mode3/timer2/uart1_mode3_timer2_12T_even_parity_example.c
@@ -0,0 +1,29 @@
+/**
+ * How to send data with uart1 in mode3 with even parity bit
+ * 
+ */
+
+#include <uart1_mode3_timer2_12T.h>
+
+#include <delay.h>
+
+#define LED P10
+
+void main()
+{
+    uart1_mode3_timer2_12T_init(RxD_P30_TxD_P31);
+    uart1_mode3_timer2_12T_start(baudrate_9600);
+
+    while (1)
+    {
+        LED = !LED;
+
+        uart1_send_byte(' ', PARITY_EVEN);
+        uart1_send_byte('O', PARITY_EVEN);
+        uart1_send_byte('k', PARITY_EVEN);
+        uart1_send_byte('\r', PARITY_EVEN);
+        uart1_send_byte('\n', PARITY_EVEN);
+
+        delay_ms(500);
+    }
+}
