0e70f6c068baf3594a7e8c824e8965ec5051b748
Michael Golovanov <mike.golovanov@gmail.com>
2026-02-23 09:46:12 +0300

    replace custom print_str to printf_tiny

--- utils/memory_dumper/mem_dump.c
+++ utils/memory_dumper/mem_dump.c
@@ -5,8 +5,10 @@
  */
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include <STC15Fxx.h>
+#include <uart.h>
+
 
 #define START_DUMP_ADDR 0x00
 #define DUMP_SIZE 256
@@ -28,25 +30,9 @@ void hex_to_string(uint8_t hex, char str_buffer[3])
     str_buffer[2] = 0;
 }
 
-void print_str(char *str)
-{
-    while (*str != 0)
-    {
-        // uart1_send_byte(*str);
-        SBUF = *str;
-	    while(TI==0);
-	    TI=0;
-        str++;
-    }
-}
-
 void main()
 {
-    // uart1_init(115200);
-    T2H = 0xFF;
-    T2L = 0xE7;
-    SCON = 0x50;
-    AUXR |= 0x15;
+    uart1_init(115200);
 
     const uint8_t row_count = DUMP_SIZE / ROW_LENGTH;
 
@@ -56,27 +42,25 @@ void main()
         char str_buffer[3];
 
         // print dump header
-        print_str("      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n");
+        printf_tiny("      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n");
 
         for(uint8_t row = 0; row < row_count; row++)
         {
             hex_to_string(address, str_buffer);
-            print_str("0x");
-            print_str(str_buffer);
-            print_str(" ");
+            printf_tiny("0x%s ", str_buffer);
             for(uint8_t column = 0; column < ROW_LENGTH; column++)
             {
                 __idata uint8_t *data_address;
                 data_address = (__idata uint8_t *) address;
                 hex_to_string(*data_address, str_buffer);
-                print_str("|");
-                print_str(str_buffer);
+                printf_tiny("|");
+                printf_tiny("%s", str_buffer);
                 address++;
             }
-            print_str("|\r\n");
+            printf_tiny("|\r\n");
         }
 
         // print dump footer
-        print_str("      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n");
+        printf_tiny("      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n");
     }
 } 
