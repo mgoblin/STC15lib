445e87d77ae897c0ce3bb7c8cc3a3bf40b593c0c
Michael Golovanov <mike.golovanov@gmail.com>
2026-02-24 22:04:08 +0300

    csv2hex. Read input file to flat list

--- utils/csv2hex/src/csv2hex/csv2hex.py
+++ utils/csv2hex/src/csv2hex/csv2hex.py
@@ -1,11 +1,49 @@
+import argparse
+import csv
+import io
 from intelhex import IntelHex
 
+def parse_agrs():
+    parser = argparse.ArgumentParser(description='Convert csv to hex file')
+
+    parser.add_argument(
+        '-i', 
+        type=argparse.FileType('r'), 
+        help='input csv file',
+        default='input.csv'
+    )
+    parser.add_argument(
+        '-o', 
+        type=argparse.FileType('w'), 
+        help='output hex file',
+        default='output.hex'
+    )
+
+    args = parser.parse_args()
+    return args
+
+def read_csv_content(f):
+    try:
+        csv_content = f.read()
+        return(csv_content)
+    finally:
+        # Ensure the file is closed
+        f.close()
+
 def main():
-    ih = IntelHex()
-    ih[0x0000] = 0x01
-    ih[0x0001] = 0x02
-    ih[0x0002] = 0x03
-    ih.write_hex_file("test.hex")
+    args = parse_agrs()
+    csv_content = read_csv_content(args.i)
+
+    print(csv_content)
+
+    string_io = io.StringIO(csv_content)
+    csvreader = csv.reader(string_io)
+
+    parsed_data = []
+    for row in csvreader:
+        parsed_data.append(row)
+
+    print(sum(parsed_data, []))
 
 if __name__ == "__main__":
     main()
