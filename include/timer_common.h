e162ee49cf552b7c777029ec74612121f1502bd1
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-10 15:04:04 +0300

    Timer common module docs structured

--- include/timer_common.h
+++ include/timer_common.h
@@ -12,7 +12,10 @@
 #include <bits.h>
 #include <timer_structs.h>
 
-//============================== Timer0 get mode, divider, pins declarations start ========
+/** @name Timer0
+ *  Timer0 routines 
+ */
+///@{
 /**
  * @brief Get timer0 mode
  * 
@@ -30,16 +33,12 @@
  * @ingroup timer_common
  */
 #define get_timer0_clock_divider() (test_if_bit_cleared(AUXR, SBIT7) ? T12 : T1)
+///@}
 
-/**
- * @brief Enable output of meandr with timer interval on P3.5 pin.
- * @details By default output is disabled
- * 
- * @param enable bool if true output is enabled otherwise output is disabled 
- * 
- * @ingroup timer0
+/** @name Timer2
+ *  Timer2 routines 
  */
-
+///@{
 /**
  * Get timer2 mode
  * 
@@ -57,5 +56,6 @@
  * @ingroup timer_common
  */
 #define get_timer2_clock_divider() (test_if_bit_set(AUXR, SBIT2) ? T1 : T12)
+///@}
 
 #endif
