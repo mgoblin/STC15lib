5deac6bae0a3e92aff6318d8eb6f712d9b9be781
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-10 13:35:54 +0300

    timer2_ms2ticks_example documented

--- examples/timer/common/timer2_ms2ticks_example.c
+++ examples/timer/common/timer2_ms2ticks_example.c
@@ -1,15 +1,37 @@
+/**
+ * This example shows how to use the timer2_ms_to_ticks 
+ * to calculate the number of timer 2 ticks for a given duration.
+ * 
+ * The duration is specified in milliseconds.
+ * If the specified duration divided by the interrupt scaling factor 
+ * can be serviced by the timer2 - its started and the LED is blinking.
+ * 
+ * Otherwise uart1 initialized and error message send through UART. 
+ */
+
 #include <timer2_mode0.h>
 #include <timer2_to_ms.h>
 
 #include <uart.h>
 #include <stdio.h>
 
+// LED pin
 #define LED P10
-#define ISR_SCALER 10
-#define DURATION_MS 50
 
+// ISR count up to this scaler
+#define ISR_SCALER 10
+
+// LED light on duration
+#define DURATION_MS 500
+
+// ISR scaler current value
 uint8_t isr_scaler_value = 0;
 
+/*
+ * Timer2 interrupt request handler
+ * 
+ * Counts Timer2 interrupts and  flip the LED state every ISR_SCALER times.
+ */
 void timerISR() __interrupt(INTERRUPT_TIMER2)
 {
     if (isr_scaler_value == ISR_SCALER)
@@ -26,15 +48,26 @@ void timerISR() __interrupt(INTERRUPT_TIMER2)
 void main()
 {
     timer2_mode0_12T_init();
-    uint16_t ticks = timer2_ms_to_ticks(DURATION_MS);
+    // Calculate timer2 ticks count given duration
+    // ticks_count is equals to 0 if given duration can not be serviced.
+    uint16_t ticks = timer2_ms_to_ticks(DURATION_MS / ISR_SCALER);
     
-    if (ticks > 0) { timer2_mode0_start(ticks); }
+    // start timer2 if it supports given duration
+    if (ticks > 0) 
+    { 
+        timer2_mode0_start(ticks); 
+    } 
+    // otherwise init uart1 for sending error message
+    else 
+    {
+        uart1_init(9600);
+    }
     
     while (1) 
     {
+        // If given duration not supported - send error message through UART
         if (ticks == 0)
         {
-            uart1_init(9600);
             printf_tiny("Duration %u cannot be serviced by timer2\r\n", DURATION_MS);
         }
     }
