129f5328e0ea5049d066687d7303340dff94a665
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-13 07:45:11 +0300

    timer0_mode1_ms_to_ticks_example added

--- examples/timer/common/timer0_mode1_ms_to_ticks_example.c
+++ examples/timer/common/timer0_mode1_ms_to_ticks_example.c
@@ -1,25 +1,25 @@
 /**
- * @file timer0_mode0_ms_to_ticks_example.c
+ * @file timer0_mode1_ms_to_ticks_example.c
  * @brief Example demonstrating conversion from milliseconds to timer0 ticks
  * 
  * This example shows how to convert a time duration in milliseconds to the
- * corresponding number of timer ticks for Timer0 in Mode 0 (16-bit timer mode).
+ * corresponding number of timer ticks for Timer0 in Mode 1.
  * 
- * The example configures Timer0 to run in 1T mode0 (one clock cycle per count)
+ * The example configures Timer0 to run in 1T mode1 (one clock cycle per count)
  * and calculates the number of ticks needed for a 3ms delay using the
- * timer0_mode0_ms_to_ticks() function. It then starts the timer and outputs the
+ * timer0_mode1_ms_to_ticks() function. It then starts the timer and outputs the
  * calculated tick value over UART.
  * 
  * @note The actual tick value will depend on the system clock frequency
  *       defined in sys.h (MAIN_Fosc). With the default 11.0592MHz clock,
  *       3ms corresponds to approximately 33,177 ticks.
  * 
- * @see timer0_mode0.h - Header for Timer0 Mode 0 functions
- * @see timer0_mode0_to_ms.h - Header containing ms to ticks conversion functions
+ * @see timer0_mode1.h - Header for Timer0 Mode 0 functions
+ * @see timer0_mode1_to_ms.h - Header containing ms to ticks conversion functions
  * 
  */
 
-#include <timer0_mode0_to_ms.h>
+#include <timer0_mode1_to_ms.h>
 
 #include <uart.h>
 #include <stdio.h>
@@ -34,19 +34,15 @@ void main()
 {
     // Initialize Timer0 in Mode 0 (16-bit timer) with 1T timing
     // In 1T mode, the timer increments once per system clock cycle
-    timer0_mode0_1T_init();
+    timer0_mode1_1T_init();
     
-    // Enable Timer0 output on P3.5 pin
-    // This will show the timer signal on the pin (useful for debugging with oscilloscope)
-    timer0_mode0_enable_P35_output();
-
     // Convert 3 milliseconds to the corresponding number of timer ticks
     // This calculation takes into account the system clock frequency and timer mode
-    uint16_t ticks = timer0_mode0_ms_to_ticks(DURATION_MS);
+    uint16_t ticks = timer0_mode1_ms_to_ticks(DURATION_MS);
     
     // Start Timer0 with the calculated number of ticks
     // The timer will count down from this value and generate an overflow interrupt
-    timer0_mode0_start(ticks);   
+    timer0_mode1_start(ticks);   
     
     // Initialize UART1 at 9600 baud rate for serial communication
     uart1_init(9600);
