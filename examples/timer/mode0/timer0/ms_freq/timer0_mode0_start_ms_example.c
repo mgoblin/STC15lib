eb94643b16123f66a051e5604bd95f27b3a7acc6
Michael Golovanov <mike.golovanov@gmail.com>
2026-06-08 08:26:03 +0300

    timer0_mode0_start_ms_example added

--- examples/timer/mode0/timer0/ms_freq/timer0_mode0_start_ms_example.c
+++ examples/timer/mode0/timer0/ms_freq/timer0_mode0_start_ms_example.c
@@ -1,4 +1,32 @@
+#include <timer0_mode0_to_ms.h>
+
+#include <uart.h>
+#include <stdio.h>
+
+#define LED P10
+#define TIMER_DURATION_MS 50
+#define LED_BLINK_SCALE 10
+
+uint8_t isr_count = 0;
+void timerISR() __interrupt(INTERRUPT_TIMER0)
+{
+    isr_count++;
+    if (isr_count == LED_BLINK_SCALE) 
+    {
+        LED = !LED;
+        isr_count = 0;
+    }
+}
 void main(void)
 {
-    
+    uart1_init(9600);
+
+    timer0_mode0_12T_init();
+
+    uint16_t ticks = timer0_mode0_ms_to_ticks(TIMER_DURATION_MS);
+    timer0_mode0_start_ms(TIMER_DURATION_MS);
+
+    while (1) {
+        printf_tiny("Ticks = %u\r\n", ticks);
+    }
 }
