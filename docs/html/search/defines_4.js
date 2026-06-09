a7fd431cacb9ebf226819b09e86cf28be5840b9b
Michael Golovanov <mike.golovanov@gmail.com>
2026-06-09 20:42:34 +0300

    Timer0 mode1 start on freq routines implemented

--- docs/html/search/defines_4.js
+++ docs/html/search/defines_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['isr_5fscale_0',['ISR_SCALE',['../timer0__mode0__start__Hz__example_8c.html#ae8355f0d397b3984ea9145959c03f49b',1,'timer0_mode0_start_Hz_example.c']]]
+  ['isr_5fscale_0',['isr_scale',['../timer0__mode0__start__Hz__example_8c.html#ae8355f0d397b3984ea9145959c03f49b',1,'ISR_SCALE:&#160;timer0_mode0_start_Hz_example.c'],['../timer0__mode1__start__Hz__example_8c.html#ae8355f0d397b3984ea9145959c03f49b',1,'ISR_SCALE:&#160;timer0_mode1_start_Hz_example.c']]]
 ];
