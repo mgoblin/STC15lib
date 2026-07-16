4b560b3b12863cd03dd18a04c2378e2eb6ecf280
Michael Golovanov <mike.golovanov@gmail.com>
2026-07-16 02:29:15 -0400

    build_summary.txt created on Platformio build

--- pio_examples_build_summary.py
+++ pio_examples_build_summary.py
@@ -25,30 +25,30 @@ def find_mem_files(examples_dir: str) -> list[str]:
 examples_dir = os.path.join(env.subst("$BUILD_DIR"), "examples")
 
 def build_summary_action(source, target, env):
-    print(target[0].get_abspath())
+    output_file = target[0].get_abspath()
 
     # Find all mem files in examples dir. Mem files contains hex size info
     mem_files_list = find_mem_files(examples_dir)
     # Regex pattern to search for hex file size
     pattern = r"ROM/EPROM/FLASH\s+[a-fx\d]+\s+[a-fx\d]+\s+(\d+).*"
 
-    print()
-    print(f"{len(mem_files_list)} .hex files and their sizes:")
+    with open(output_file, 'w') as out_fp:
+        out_fp.write(f"{len(mem_files_list)} .hex files and their sizes:")
+        # Iterate through mem files list and print size of each hex file
+        for mem_file_name in sorted(mem_files_list):
+            with open(mem_file_name.strip()) as mem_file:
+                # hex and mem file names are the same
+                hex_name = os.path.basename(mem_file_name).split('.')[0] + ".hex"
+
+                mem_file_content = mem_file.read()
+                matches = re.search(pattern, mem_file_content, re.MULTILINE)
+                if matches:
+                    out_fp.write(f"{hex_name:<60}{matches.group(1)} bytes\n")
+                else:
+                    out_fp.write(f"{hex_name:<60}No size found\n")
+
     
-    # Iterate through mem files list and print size of each hex file
-    for mem_file_name in sorted(mem_files_list):
-        with open(mem_file_name.strip()) as mem_file:
-            # hex and mem file names are the same
-            hex_name = os.path.basename(mem_file_name).split('.')[0] + ".hex"
-
-            mem_file_content = mem_file.read()
-            matches = re.search(pattern, mem_file_content, re.MULTILINE)
-            if matches:
-                print(f"{hex_name:<60}{matches.group(1)} bytes")
-            else:
-                print(f"{hex_name:<60}No size found\n")
-
-
+#  TODO print build summary
 
 post_action = env.Command(
     target = f"{examples_dir}/build_summary.txt",
