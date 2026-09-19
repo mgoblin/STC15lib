d99d6f763c133e9feba7a046f0a649e8d3d68266
Michael Golovanov <mike.golovanov@gmail.com>
2026-09-19 10:51:52 +0300

    Move doxygen.py to platformio_build directory

--- platformio_build/doxygen.py
+++ platformio_build/doxygen.py
@@ -2,6 +2,8 @@ Import("env")
 import os
 
 def post_build_action(source, target, env):
+    print("Generating Doxygen documentation")
     os.system("doxygen doxygen.conf")
+    print("Generating Doxygen documentation finished")
 
 env.AddPostAction("buildprog", post_build_action)    
